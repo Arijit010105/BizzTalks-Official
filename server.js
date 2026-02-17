3705afb6c46c5425c184dd222ae0d5157a66586e
Arijit010105 <managerialtalk@gmail.com>
2026-02-17 14:34:44 +0530

    chore: Add debug logging and improved error alerts

--- server.js
+++ server.js
@@ -15,6 +15,10 @@ connectDB();
 // Middleware
 app.use(cors()); // Enable All CORS Requests
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    next();
+});
 app.use(express.static(path.join(__dirname, '/'))); // Serve static files
 
 // API Route
